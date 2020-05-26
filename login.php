ad753a0ac9aae525ca58341df18edde384bee9d8
jennita <jennita.aubeeluc@gmail.com>
2020-05-26 11:17:07 +0400

    fix all css path

--- login.php
+++ login.php
@@ -25,16 +25,16 @@ include_once '../conn/dbconnect.php';
     <!--
     CSS
     ============================================= -->
-    <link rel="stylesheet" href="../css/linearicons.css">
-    <link rel="stylesheet" href="../css/font-awesome.min.css">
-    <link rel="stylesheet" href="../css/bootstrap.css">
-    <link rel="stylesheet" href="../css/magnific-popup.css">
-    <link rel="stylesheet" href="../css/jquery-ui.css">
-    <link rel="stylesheet" href="../css/nice-select.css">
-    <link rel="stylesheet" href="../css/animate.min.css">
-    <link rel="stylesheet" href="../css/owl.carousel.css">
-    <link rel="stylesheet" href="../css/main.css">
-    <link href="../css/loginTechnician.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="/css/linearicons.css">
+    <link rel="stylesheet" href="/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/css/bootstrap.css">
+    <link rel="stylesheet" href="/css/magnific-popup.css">
+    <link rel="stylesheet" href="/css/jquery-ui.css">
+    <link rel="stylesheet" href="/css/nice-select.css">
+    <link rel="stylesheet" href="/css/animate.min.css">
+    <link rel="stylesheet" href="/css/owl.carousel.css">
+    <link rel="stylesheet" href="/css/main.css">
+    <link href="/css/loginTechnician.css" rel="stylesheet" type="text/css" />
   </head>
   <body>
     <header id="header">
