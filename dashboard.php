ceb38c3538d8a5e3c19deff6238e82d49b4fc282
jennita <jennita.aubeeluc@gmail.com>
2020-05-27 16:29:31 +0400

    Login succesfull and redirec to student/lecturer dashboard

--- dashboard.php
+++ dashboard.php
@@ -22,7 +22,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 </head>
 <body>
     <div class="page-header">
-        <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to your profile</h1>
+        <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["email"]); ?></b>. Welcome to your profile</h1>
     </div>
     <p>
         <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
